21e2983561bbfe717a4e6be972111e49bc25e9fc
Marcus Stoiber <marcus.stoiber@nanoporetech.com>
2025-10-01 09:58:15 -0700

    Update needletail. Also update mac build issues.

--- build.rs
+++ build.rs
@@ -1,73 +1,53 @@
+use std::{env, path::PathBuf};
 extern crate bindgen;
 
-
-use std::env;
-use std::path::PathBuf;
-use std::fs::File;
-use std::io::prelude::*;
-
-// We have path to edlib, generate wrapper.h with  path to correct include 
-fn write_wrapper(wrapper_path : &PathBuf) {
-    let mut inclpath = PathBuf::new();
-    inclpath.push("include");
-    inclpath.push("edlib.h");
-    let mut incl = String::from("#include\"");
-    incl.push_str(& inclpath.to_str().unwrap());
-    incl.push_str("\"");
-    // now we write file wrapper.h
-    let mut f = File::create(wrapper_path).unwrap();
-    f.write_all(incl.as_bytes()).expect("cannot write wrapper.h");
+fn write_wrapper(wrapper_path: &PathBuf) {
+    std::fs::write(wrapper_path, "#include \"edlib.h\"").unwrap();
 }
 
-
-
 fn main() {
-    //  try to build edlib in build.rs with edlib cloned in edlib-c (from which we removed )
-    let dst = cmake::Config::new("edlib-c")
-                .cflag("-D CMAKE_BUILD_TYPE=Release")
-                .build();
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    //
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let wrapper_path = out_path.join("wrapper.h");
-    //
-    write_wrapper(&wrapper_path);
-    // adapted fix from sam217pa. libstdc++ name varies...
-    let lib_std : &str;
-    if cfg!(target_os = "macos") {
-        lib_std  = "cargo:rustc-link-lib=c++";
-    }
-    else {
-        lib_std = "cargo:rustc-link-lib=stdc++";
-    }
-    
-    let mut libdir = String::from("cargo:rustc-link-search=");
-    libdir.push_str(out_path.to_str().unwrap());
-    libdir.push_str("/lib");
-    println!("{}",libdir);
-    println!("cargo:rustc-link-lib=edlib");
-    println!("{}", lib_std);
+    // Ensure sane macOS min version for all child toolchains (clang, cmake, bindgen clang)
+    #[cfg(target_os = "macos")]
+    env::set_var("MACOSX_DEPLOYMENT_TARGET", "13.0");
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
+    // Configure and build edlib
+    let mut cfg = cmake::Config::new("edlib-c");
+    cfg.profile("Release")
+        .define("CMAKE_BUILD_TYPE", "Release")
+        .define("CMAKE_POLICY_VERSION_MINIMUM", "3.5");
+    #[cfg(target_os = "macos")]
+    {
+        cfg.define("CMAKE_OSX_ARCHITECTURES", "arm64")
+           .define("CMAKE_OSX_DEPLOYMENT_TARGET", "13.0");
+    }
+    let dst = cfg.build(); // installs into {dst}/include and {dst}/lib
+
+    // Link the static library that CMake installed
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-lib=static=edlib");
+    if cfg!(target_os = "macos") { println!("cargo:rustc-link-lib=c++"); }
+    else { println!("cargo:rustc-link-lib=stdc++"); }
+
+    // Generate wrapper and bindings
+    let out = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let wrapper = out.join("wrapper.h");
+    write_wrapper(&wrapper);
+
+    // >>> This is the important change: include the CMake install include dir
+    let edlib_include = dst.join("include");
+
     let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header(wrapper_path.to_str().unwrap())
-        //Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
+        .header(wrapper.to_str().unwrap())
+        .clang_arg(format!("-I{}", edlib_include.display()))
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-         // Finish the builder and generate the bindings.
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    //
-    println!("cargo:rerun-if-changed=binding.rs");
+    println!("cargo:rerun-if-changed=edlib-c/include/edlib.h");
+    println!("cargo:rerun-if-changed=edlib-c/CMakeLists.txt");
+    println!("cargo:rerun-if-changed=build.rs");
+
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
-    //
+        .write_to_file(out.join("bindings.rs"))
+        .expect("Could not write bindings");
 }
